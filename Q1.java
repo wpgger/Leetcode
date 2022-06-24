77c2ff9ae9057be22f0c018c5d081ab2f225e93a
Zhuye <zche@academic.rrc.ca>
2022-06-23 22:16:41 -0500

    Q1 Update

--- Q1.java
+++ Q1.java
@@ -6,8 +6,7 @@ class Q1{
         int nums[] = {2,7,11,15};
         int target = 17;
         
-        System.out.println(Arrays.toString(test.twoSum(nums, target)));
-        
+        System.out.println(Arrays.toString(test.twoSum(nums, target)));   
     }
 }
 
@@ -20,10 +19,8 @@ class Solution1 {
         for (int i = 0; i < nums.length; i++) {
             for(int j = i+1; j < nums.length; j++){
                 if (nums[i]+nums[j] == target){
-    
                     result[0] = i;    
                     result[1] = j;
-                    
                 }
             }
         }
